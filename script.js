70bece73e20edab31a78ebe6b5fcc44b9fafe9e8
goodluck1455 <goodluckstephen237@gmail.com>
2021-04-09 18:24:53 +0100

    new commit

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-let faBars = document.querySelector(".fa-bars");
-let faTimes = document.querySelector(".fa-times");
-let navbar = document.querySelector(".navbarA ul");
+let faBars = document.querySelector('.fa-bars')
+let faTimes = document.querySelector('.fa-times')
+let navbar = document.querySelector('.navbarA ul')
+let cardNumber = document.querySelector('#cardnumber')
+let ExpiredDate = document.querySelector('#ExpiredDate')
+let securityCode = document.querySelector('#securityCode')
+let postalCode = document.querySelector('#postalCode')
 
-
-
-faBars.addEventListener("click", ()=>{
- faBars.style.display = "none";
- faTimes.style.display = "block";
- navbar.style.transform = "scaleX(1)";
-
+faBars.addEventListener('click', () => {
+  faBars.style.display = 'none'
+  faTimes.style.display = 'block'
+  navbar.style.transform = 'scaleX(1)'
 })
 
-faTimes.addEventListener("click", () => {
- faBars.style.display = "block";
- faTimes.style.display = "none";
- navbar.style.transform = "scaleX(0)";
-
+faTimes.addEventListener('click', () => {
+  faBars.style.display = 'block'
+  faTimes.style.display = 'none'
+  navbar.style.transform = 'scaleX(0)'
 })
+
+function validate() {
+  if (
+    cardNumber.value == '' ||
+    securityCode.value == '' ||
+    ExpiredDate.value == '' ||
+    postalCode.value == ''
+  ) {
+    cardNumber.style.border = '1px solid red'
+    securityCode.style.border = '1px solid red'
+    ExpiredDate.style.border = '1px solid red'
+    postalCode.style.border = '1px solid red'
+    return false
+  } else {
+    alert('Card details successfully added')
+  }
+}
+// function remove(){
+//  if (!cardNumber.value == '' || !cardNumber.value == null) {
+//    cardNumber.style.border = 'none'
+//  }
+// }
+// remove();
